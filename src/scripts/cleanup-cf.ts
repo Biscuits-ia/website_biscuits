6667060a01978ba34d3c0d16151a025e5c548f61
AlexisTak <swetoksy@gmail.com>
2026-01-24 12:45:16 +0100

    update

--- src/scripts/cleanup-cf.ts
+++
@@ -1,5 +0,0 @@
-if (typeof window !== "undefined") {
-  const url = new URL(window.location.href);
-  url.searchParams.delete("__cf_chl_rt_tk");
-  window.history.replaceState({}, "", url.toString());
-}
